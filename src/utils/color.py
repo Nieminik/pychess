09ffab4863b47c4229679538acda88d47029665c
Dominik Niemiro <dniemiro@protonmail.com>
2019-05-10 21:17:53 +0200

    Use auto from enum in color.py

--- src/utils/color.py
+++ src/utils/color.py
@@ -1,10 +1,10 @@
 """Module defining enum class for color."""
 
-from enum import Enum
+from enum import Enum, auto
 
 
 class Color(Enum):
     """Color enum class."""
 
-    WHITE = 0
-    BLACK = 1
+    WHITE = auto()
+    BLACK = auto()
