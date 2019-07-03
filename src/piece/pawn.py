0acf6157f2c1ecec5b945d9eb38c22a40f5d9e5e
Nieminik <nieminik@protonmail.com>
2019-07-03 19:38:54 +0200

    Implement pawn's range

--- src/piece/pawn.py
+++ src/piece/pawn.py
@@ -1,6 +1,7 @@
 """A pawn module."""
 
 from .piece import BasePiece
+from src.utils.color import Color
 
 
 class Pawn(BasePiece):
@@ -8,4 +9,16 @@ class Pawn(BasePiece):
 
     @property
     def range(self):  # noqa: D102
-        return
+        row, col = self.field
+        inc = 1 if self.color == Color.WHITE else -1
+        n_row = row + inc
+
+        move_range = [(n_row, col)]
+        if not self.moves:
+            move_range.append((n_row + inc, col))
+
+        for diag_field in ((n_row, col-1), (n_row, col+1)):
+            if self.grid(diag_field):
+                move_range.append(diag_field)
+
+        return move_range
