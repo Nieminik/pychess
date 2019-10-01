31f3ce0a272678ddda739089245b5dca00e566b1
Dominik Niemiro <dniemiro@protonmail.com>
2019-10-01 09:04:20 +0200

    Change the grid enemies test

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -8,12 +8,23 @@ from pychess.piece.color import Color
 
 @pytest.fixture
 def grid():  # noqa: D103
-    g = Grid()
-    g.add_piece(Piece((1, 1), color=Color.Black))
-    g.add_piece(Piece((1, 2), color=Color.White))
+    return Grid()
+    g.add_piece(
+    g.add_piece()
     return g
 
+pieces_groups = (
+    (Piece((1, 2), color=Color.White), Piece((1, 1), color=Color.Black)),
+    (Piece((2, 3), color=Color.White), Piece((4, 4), color=Color.White)),
+    (Piece((1, 2), color=Color.Black), Piece((0, 2), color=Color.Black))    
+)
 
-def test_enemies(grid):  # noqa: D103
-    for piece in grid.fields.values():
+@pytest.mark.parametrize("pieces", pieces_groups)
+def test_enemies(grid, pieces):  # noqa: D103
+    for piece in pieces:
+        grid.add_piece(piece)
+    pieces = grid.fields.values()
+    assert pieces
+    
+    for piece in pieces:
         assert grid.get_enemies(piece)
