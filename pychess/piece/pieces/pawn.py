91a591a069875058d2656ed9b4a9edfb701bffd5
Dominik Niemiro <dniemiro@protonmail.com>
2020-10-24 18:01:31 +0200

    Add pawn tests + functionality (#9)
    * Add first pawn tests
    
    * Add remaining move range tests
    
    * Add attack range test
    
    * Correct style
    
    * Implement an passant
    
    * Conduct style changes
    
    * Add first pawn tests
    
    * Add remaining move range tests
    
    * Add attack range test
    
    * Correct style
    
    * Implement an passant
    
    * Conduct style changes

--- pychess/piece/pieces/pawn.py
+++ pychess/piece/pieces/pawn.py
@@ -1,14 +1,23 @@
 """Provide pawn piece functionality."""
+from enum import Enum
+
+from pychess.piece.color import Color
 from pychess.piece.move_iters import up, down
 from pychess.piece.pieces.base import Piece
-from pychess.piece.position import Position, MAX_POS, MIN_POS
-from pychess.piece.color import Color
+from pychess.piece.position import Position
+
+
+class Direction(Enum):
+    """Pawn move direction enum."""
+
+    Down = -1
+    Up = 1
 
 
 class Pawn(Piece):
     """Represent a queen object."""
 
-    def __init__(self, position, color, grid):
+    def __init__(self, position, color=Color.White, grid=None):
         super(Pawn, self).__init__(position, color, grid)
         self.move_attacks = False
 
@@ -16,7 +25,8 @@ class Pawn(Piece):
     def move_range(self):
         """Get a move range for pawn."""
         rng = []
-        fwd_func = up if self.color == Color.White else down
+        fwd_func = {Direction.Up: up,
+                    Direction.Down: down}[self.get_direction(self)]
         fwd = fwd_func(self)
         for pos, move_cond in zip(fwd, (True, not self.moves)):
             if pos and move_cond:
@@ -28,11 +38,41 @@ class Pawn(Piece):
     def attack_range(self):
         """Get an attack range for pawn."""
         row, col = self.position
-        n_row = row + (1 if self.color is Color.White else -1)
+        n_row = row + self.get_direction(self).value
         rng = []
 
         for n_col in (col + 1, col - 1):
-            if n_col in range(MIN_POS, MAX_POS):
-                rng.append(Position(n_row, n_col))
+            pos = Position(n_row, n_col)
+            other = self.grid[pos]
+            if pos.is_valid() and other and other.color != self.color:
+                rng.append(pos)
+
+            an_passant_pos = Position(row, n_col)
+            other = self.grid[an_passant_pos]
+            an_passant = (other and other.moves == 1)
+            if pos.is_valid() and an_passant and other.color != self.color:
+                rng.append(pos)
 
         return rng
+
+    def move(self, value):
+        """Move, check for an passant capture, report it."""
+        new_pos = Position(*value)
+        other = self.grid[new_pos]
+        report = False
+        if new_pos in self.attack_range and other is None:
+            report = True
+            r, c = new_pos
+            curr_r, curr_c = self.position
+            other = self.grid[Position(curr_r, c)]
+
+        move_succeeded = super().move(value)
+        if report and move_succeeded:
+            self.grid.report_capture(other)
+
+        return move_succeeded
+
+    @staticmethod
+    def get_direction(pawn):
+        """Get direction of pawn."""
+        return Direction.Up if pawn.color is Color.White else Direction.Down
