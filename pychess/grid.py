27d707c34efbdec68d6404d882a788504062bb49
Dominik Niemiro <dniemiro@protonmail.com>
2021-02-06 16:19:51 +0100

    Rework grid.py (#12)
    * Rework grid.py
    
    * Add missing defaultdict import
    
    * Correct self.piece usage
    
    * Correct fields property
    
    * Correct report_capture
    
    * Correct move test

--- pychess/grid.py
+++ pychess/grid.py
@@ -1,25 +1,38 @@
 """Module implementing grid functionality."""
 
+from itertools import chain
+from collections import defaultdict
+
 
 class Grid(object):
     """A class for containing pieces."""
 
     def __init__(self):  # noqa: D103
-        self.fields = {}
+        self._pieces = {}
         self.captured = []
 
     def __getitem__(self, item):  # noqa: D105
-        return self.fields.get(item, None)
+        return self.fields[item]
+
+    @property
+    def pieces(self):
+        return chain.from_iterable(self._pieces.values())
+    
+    @property
+    def fields(self):
+        return defaultdict(lambda: None, {p.position: p for p in self.pieces})
 
     def add_piece(self, piece):
         """Add a piece to the grid."""
         piece.grid = self
-        self.fields[piece.position] = piece
+        cls_pieces = self._pieces.setdefault(piece.__class__, [])
+        cls_pieces.append(piece)
 
     def get_enemies(self, piece):
         """Get enemies of given piece."""
         inverted_color = piece.color.inverted()
-        return [x for x in self.fields.values() if x.color is inverted_color]
+        
+        return [x for x in self.pieces if x.color is inverted_color]
 
     def move(self, old_pos, new_pos):
         """Move piece."""
@@ -27,10 +40,6 @@ class Grid(object):
         other = self[new_pos]
 
         move_successful = piece.move(new_pos)
-
-        if move_successful:
-            del self.fields[old_pos]
-            self.fields[new_pos] = piece
 
         if other and move_successful:
             self.report_capture(other)
@@ -40,6 +49,4 @@ class Grid(object):
     def report_capture(self, piece):
         """Keep track of captured pieces."""
         self.captured.append(piece)
-        for pos, grid_piece in dict(self.fields).items():
-            if piece == grid_piece:
-                del self.fields[pos]
+        self._pieces[piece.__class__].remove(piece)
