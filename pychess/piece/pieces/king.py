1bbc9a87eee54eb4cc01e0837dcf23ef2c3229ed
Dominik Niemiro <dniemiro@protonmail.com>
2021-02-07 02:01:24 +0100

    Correct king attack_range infinite recursion loop

--- pychess/piece/pieces/king.py
+++ pychess/piece/pieces/king.py
@@ -11,12 +11,16 @@ class King(Piece):
     @property
     def move_range(self):
         """Get a move range for king."""
+        pieces = self.grid.get_enemies(self)
+        ranges = list(chain.from_iterable([p.attack_range for p in pieces]))
+
+        return [x for x in self.attack_range if x not in ranges]
+
+    @property
+    def attack_range(self):
+        """Get an attack range for king."""
         hor = horizontal(self, 2)
         ver = vertical(self, 2)
         diag = diagonal(self, 4)
 
-        pieces = self.grid.get_enemies(self)
-        ranges = list(chain.from_iterable([p.attack_range for p in pieces]))
-
-        rng = list(hor) + list(ver) + list(diag)
-        return [x for x in rng if x not in ranges]
+        return list(hor) + list(ver) + list(diag)
