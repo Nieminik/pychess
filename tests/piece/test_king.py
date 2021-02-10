58415d3f06d48e6205b014743e96bd343796f768
Dominik Niemiro <nieminik@protonmail.com>
2021-02-10 21:31:49 +0100

    Add remaining king test

--- tests/piece/test_king.py
+++ tests/piece/test_king.py
@@ -3,8 +3,9 @@
 import pytest
 
 from pychess.grid import Grid
+from pychess.piece.color import Color
 from pychess.piece.position import Position, MAX_POS
-from pychess.piece.pieces import King
+import pychess.piece.pieces as pieces
 
 from pychess.piece.move_iters import diagonal, horizontal, vertical
 
@@ -23,7 +24,7 @@ INVALID_MOVES = [(0, 2), (0, -2), (2, 0), (-2, 0), (2, 2), (-2, -2)]
 @pytest.fixture
 def king():  # noqa: D103
     grid = Grid()
-    king = King(Position(1, 1))
+    king = pieces.King(Position(1, 1))
     grid.add_piece(king)
     return king
 
@@ -49,3 +50,40 @@ def test_king_incorrect_move(wrong_move_pos, king):  # noqa: D103
     assert not king.move(Position(-1, king.position.col))
     assert not king.move(Position(king.position.row, MAX_POS))
     assert not king.move(Position(king.position.row, -1))
+
+
+@pytest.mark.parametrize("color", (Color.White, Color.Black))
+def test_king_attacked_move_fields(color, king):  # noqa: D103
+    grid = king.grid
+    king.color = color
+
+    start_move_range = set(king.move_range)
+
+    rook = pieces.Rook(
+        Position(king.position.row + 2, king.position.col + 1),
+        color=color.inverted())
+    grid.add_piece(rook)
+
+    expected_range = start_move_range - set(rook.attack_range)
+
+    assert king.move_range != king.attack_range
+    assert sorted(king.move_range) == sorted(expected_range)
+
+    rook.color = rook.color.inverted()
+    assert sorted(king.move_range) != sorted(expected_range)
+    rook.color = rook.color.inverted()
+
+    bishop = pieces.Bishop(
+        Position(king.position.row, king.position.col + 1),
+        color=color)
+    grid.add_piece(bishop)
+
+    # bishop now blocks two fields of rook's attack range
+    expected_range.add(Position(bishop.position.row - 1, bishop.position.col))
+    assert sorted(king.move_range) == sorted(expected_range)
+
+    bishop.color = bishop.color.inverted()
+    expected_range = expected_range - set(bishop.attack_range) 
+    expected_range.add(Position(bishop.position.row, bishop.position.col))
+
+    assert sorted(king.move_range) == sorted(expected_range)
